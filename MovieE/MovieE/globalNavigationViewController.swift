262da61d5e9e778e74b2d52c26158a8b2adf3bf9
heyner-2001 <heyner.rodriguez@grability.com>
2021-07-02 21:22:57 -0500

    Add external dependencies and add first files.

---
+++ MovieE/MovieE/globalNavigationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  globalNavigationViewController.swift
+//  MovieE
+//
+//  Created by Heyner Rodriguez on 2/07/21.
+//
+
+import UIKit
+var globalNavigationViewController: UINavigationController?
